4c7c1abb6af6cf6093307df9aa5aaf3e38cf8423
Eyüp Furkan Tüylü <eyupfurkantuylu@gmail.com>
2025-09-30 23:54:22 +0300

    Product Detail Service added.

---
+++ MultiShop/MultiShop.Catalog/Services/ProductDetailServices/IProductDetailService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShop.Catalog.Dtos.ProductDetailDtos;
+
+namespace MultiShop.Catalog.Services.ProductDetailServices;
+
+public interface IProductDetailService
+{
+    Task<List<ResultProductDetailDto>> GetAllProductDetailAsync();
+    Task CreateProductDetailAsync(CreateProductDetailDto createProductDetailDto);
+    Task UpdateProductDetailAsync(UpdateProductDetailDto updateProductDetailDto);
+    Task DeleteProductDetailAsync(string ProductDetailId);
+    Task<GetByIdProductDetailDto> GetByIdProductDetailAsync(string ProductDetailId);
+}
