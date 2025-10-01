f4d9fd5c9646b74d4eeb653c44cbbfe087696318
Eyüp Furkan Tüylü <eyupfurkantuylu@gmail.com>
2025-10-01 15:53:51 +0300

    Created Order Detail CQRS methods.

---
+++ MultiShop/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDeteailHandlers/GetOrderDetailByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
+using MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDeteailHandlers;
+public class GetOrderDetailByIdQueryHandler
+{
+    private readonly IRepository<OrderDetail> _repository;
+
+    public GetOrderDetailByIdQueryHandler(IRepository<OrderDetail> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<GetOrderDetailByIdQueryResult> Handle(GetOrderDetailByIdQuery query)
+    {
+        var result = await _repository.GetByIdAsync(query.Id);
+        return new GetOrderDetailByIdQueryResult
+        {
+            OrderDetailId = result.OrderDetailId,
+            ProductId = result.ProductId,
+            ProductName = result.ProductName,
+            ProductPrice = result.ProductPrice,
+            ProductAmount = result.ProductAmount,
+            ProductTotalPrice = result.ProductTotalPrice,
+            OrderingId = result.OrderingId
+        };
+    }
+}
---
+++ MultiShop/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDeteailHandlers/GetOrderDetailQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDeteailHandlers;
+public class GetOrderDetailQueryHandler
+{
+    private readonly IRepository<OrderDetail> _repositroy;
+
+    public GetOrderDetailQueryHandler(IRepository<OrderDetail> repositroy)
+    {
+        _repositroy = repositroy;
+    }
+
+    public async Task<List<GetOrderDetailByIdQueryResult>> Handle()
+    {
+        var values = await _repositroy.GetAllAsync();
+        return values.Select(x => new GetOrderDetailByIdQueryResult
+        {
+            OrderDetailId = x.OrderDetailId,
+            ProductId = x.ProductId,
+            ProductName = x.ProductName,
+            ProductPrice = x.ProductPrice,
+            ProductAmount = x.ProductAmount,
+            ProductTotalPrice = x.ProductTotalPrice,
+            OrderingId = x.OrderingId
+        }).ToList();
+    }
+}
