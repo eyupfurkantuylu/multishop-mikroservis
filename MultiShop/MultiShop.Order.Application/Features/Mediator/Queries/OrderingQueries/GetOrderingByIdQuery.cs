f7f4d2fc24af16ae67685c3cf32506304bda6497
Eyüp Furkan Tüylü <eyupfurkantuylu@gmail.com>
2025-10-03 22:40:47 +0300

    yedek

---
+++ MultiShop/MultiShop.Order.Application/Features/Mediator/Queries/OrderingQueries/GetOrderingByIdQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+public class GetOrderingByIdQuery : IRequest<GetOrderingByIdQuery>
+{
+    public int Id { get; set; }
+
+    public GetOrderingByIdQuery(int id)
+    {
+        Id = id;
+    }
+}
