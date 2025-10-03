76e8c130bf5d85e06423c0b4b15da07eb567da3e
Eyüp Furkan Tüylü <eyupfurkantuylu@gmail.com>
2025-10-03 22:49:03 +0300

    Added Mediatr nuget. Created Ordering Mediatr classes.

--- MultiShop/MultiShop.Order.Application/Features/Mediator/Commands/OrderingCommands/RemoveOrderingCommand.cs
+++ MultiShop/MultiShop.Order.Application/Features/Mediator/Commands/OrderingCommands/RemoveOrderingCommand.cs
@@ -6,10 +6,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
-public class UpdateOrderingCommand : IRequest
+public class RemoveOrderingCommand : IRequest
 {
-    public int OrderingId { get; set; }
-    public string UserId { get; set; }
-    public decimal TotalPrice { get; set; }
-    public DateTime OrderDate { get; set; }
+    public int Id { get; set; }
+
+    public RemoveOrderingCommand(int id)
+    {
+        Id = id;
+    }
 }
